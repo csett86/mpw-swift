fcf32889990d3649596ea30d4c9d68879327d312
Christoph Settgast <csett86_git@quicksands.de>
2026-06-04 17:48:12 +0200

    works on simulator

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "SpectreCore",
     platforms: [
         .macOS(.v14),
-        .iOS(.v16)
+        .iOS(.v17)
     ],
     products: [
         .library(name: "SpectreCore", targets: ["SpectreCore"])
